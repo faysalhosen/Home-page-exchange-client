d829f6797ad29b1b88e846489318fe3c55bd3ba6
faysalhosen <faysanhosen7025@gmail.com>
2023-11-18 14:51:37 +0600

    register page added

---
+++ src/FireBase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDT_7npD02akVSsfnN7YPW5oxwepO1y4ac",
+  authDomain: "home-page-exchange.firebaseapp.com",
+  projectId: "home-page-exchange",
+  storageBucket: "home-page-exchange.appspot.com",
+  messagingSenderId: "942956500353",
+  appId: "1:942956500353:web:3c8cf1463af671d8218480"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
