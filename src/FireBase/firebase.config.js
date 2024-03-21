6bc5ad47996a277b516ee460f2b3d38ebf142191
faysalhosen <faysalhosen7025@gmail.com>
2024-03-21 17:37:44 +0600

    Update: readme file

--- src/FireBase/firebase.config.js
+++ src/FireBase/firebase.config.js
@@ -1,3 +1,23 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_apiKey,
+//   authDomain: import.meta.env.VITE_authDomain,
+//   projectId: import.meta.env.VITE_projectId,
+//   storageBucket: import.meta.env.VITE_storageBucket,
+//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
+//   appId: import.meta.env.VITE_appId,
+//   measurementId : import.meta.env.VITE_measurementId
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export default app;
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -5,15 +25,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
-  measurementId : import.meta.env.VITE_measurementId
+  apiKey: "AIzaSyBUbHjszevyhZgiy7HfW2K9TLc9ypg94xo",
+  authDomain: "home-page-exchange-7e278.firebaseapp.com",
+  projectId: "home-page-exchange-7e278",
+  storageBucket: "home-page-exchange-7e278.appspot.com",
+  messagingSenderId: "584529245087",
+  appId: "1:584529245087:web:88a0b3157606857a7843aa"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export default app;
+ const app = initializeApp(firebaseConfig);
+ export default app;
